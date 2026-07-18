a3f7643fe98ee779973682b8fc7392ac492218ad
Shannon Bray (NC) <108833174+ShannonBrayNC@users.noreply.github.com>
2026-07-18 19:03:35 -0400

    Add search API contract exports

---
+++ src/kre/schemas/__init__.py
@@ -0,0 +1,15 @@
+from kre.schemas.search import (
+    SearchChannelScore,
+    SearchHit,
+    SearchMode,
+    SearchRequest,
+    SearchResponse,
+)
+
+__all__ = [
+    "SearchChannelScore",
+    "SearchHit",
+    "SearchMode",
+    "SearchRequest",
+    "SearchResponse",
+]
