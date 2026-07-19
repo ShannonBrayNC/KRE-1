68d461d6436cd893e7fa2a8e50777505847121a6
Shannon Bray (NC) <108833174+ShannonBrayNC@users.noreply.github.com>
2026-07-19 01:28:40 -0400

    Export PostgreSQL migration contracts

--- src/kre/storage/__init__.py
+++ src/kre/storage/__init__.py
@@ -1,5 +1,10 @@
 from kre.storage.base import KnowledgeRepository
 from kre.storage.memory import InMemoryKnowledgeRepository
+from kre.storage.migrations import (
+    PostgresMigration,
+    PostgresMigrationRunner,
+    initial_postgres_migrations,
+)
 from kre.storage.postgres import PostgresKnowledgeRepository
 from kre.storage.postgres_schema import PostgresSchemaConfig, render_postgres_schema
 
@@ -7,6 +12,9 @@ __all__ = [
     "InMemoryKnowledgeRepository",
     "KnowledgeRepository",
     "PostgresKnowledgeRepository",
+    "PostgresMigration",
+    "PostgresMigrationRunner",
     "PostgresSchemaConfig",
+    "initial_postgres_migrations",
     "render_postgres_schema",
 ]
