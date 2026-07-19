8ac3817851ccd5c1e6574af47579ad796eee66be
Shannon Bray (NC) <108833174+ShannonBrayNC@users.noreply.github.com>
2026-07-18 23:37:13 -0400

    feat: export PostgreSQL schema contract

--- src/kre/storage/__init__.py
+++ src/kre/storage/__init__.py
@@ -1,4 +1,10 @@
 from kre.storage.base import KnowledgeRepository
 from kre.storage.memory import InMemoryKnowledgeRepository
+from kre.storage.postgres_schema import PostgresSchemaConfig, render_postgres_schema
 
-__all__ = ["InMemoryKnowledgeRepository", "KnowledgeRepository"]
+__all__ = [
+    "InMemoryKnowledgeRepository",
+    "KnowledgeRepository",
+    "PostgresSchemaConfig",
+    "render_postgres_schema",
+]
